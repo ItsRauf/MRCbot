20d6284749a22d5207c886628b9406df5a6a1a80
B <benricheson101@gmail.com>
2020-04-08 11:26:42 -0400

    Added the ability for role names and mentions to be valid input.
    Note: I had to remove the 'reason' parameter to for it to work.

--- src/cmds/setcolor.ts
+++ src/cmds/setcolor.ts
@@ -1,14 +1,34 @@
 import { Command } from './Command';
-import { MessageEmbed } from 'discord.js';
+import { MessageEmbed, Message, Role } from 'discord.js';
+import { findBestMatch } from 'string-similarity';
 
 function validateHexColor(color: string): boolean {
   const regex = /[0-9A-Fa-f]{6}/gm;
   return regex.test(color);
 }
 
+async function findRole(msg: Message, role: string) {
+  const guildRole =
+    msg.guild?.roles.cache.get(role) ||
+    msg.guild?.roles.cache.find(
+      r => r.name.toLowerCase() === role.toLowerCase()
+    );
+  if (guildRole) return guildRole;
+  // Match the nearest role name
+  const guildRoles: Role[] | undefined = msg.guild?.roles.cache.array();
+  if (!guildRoles) return null;
+  const roleArray: string[] = guildRoles.map(r => r.name.toLowerCase());
+  const {
+    bestMatchIndex,
+    bestMatch: { rating },
+  } = findBestMatch(role.toLowerCase(), roleArray);
+  if (rating < 0.3) return null;
+  return guildRoles[bestMatchIndex];
+}
+
 export const cmd = new Command(
   'setcolor',
-  async (MRC, msg, [rID, hexColor, ...reason]) => {
+  async (MRC, msg, [hexColor, ...rID]) => {
     if (!msg.member?.permissions.has('MANAGE_ROLES')) {
       // Do Nothing
     } else if (!rID) {
@@ -18,9 +38,12 @@ export const cmd = new Command(
     } else if (!validateHexColor(hexColor)) {
       msg.channel.send('Invalid parameter: `hex code`');
     } else {
-      const role = await msg.guild?.roles.fetch(rID);
+      const role = await findRole(msg, rID.join(' '));
       if (role) {
-        role.setColor(hexColor, reason.join(' '));
+        role.setColor(
+          hexColor,
+          `Role color changed on behalf of ${msg.author.tag} at ${new Date()}`
+        );
         msg.channel.send(
           `Role \`${role.name} (${role.id})\` is now set to color \`${hexColor}\``
         );
@@ -36,7 +59,7 @@ export const cmd = new Command(
   },
   {
     description: "Change a role's color to a specified hex code",
-    usage: ['setcolor {rold id} {hex code} [reason]'],
+    usage: ['setcolor {hex code} {role name | id | mention}'],
     aliases: ['sc', 'setcol'],
   }
 );
